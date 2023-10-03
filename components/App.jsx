4cb55ac05f582949a0d3c6b07c9a94a980ba7170
JDalmedo <jjdalmedo@gmail.com>
2023-10-03 21:15:49 +0200

    first

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import Axios from "axios"
-
 import './App.css'
 
 const App = () => {
   const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
   const [pokemon, setPokemon] = useState([]);
     const searchPokemon = () => {
       Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
@@ -20,19 +20,42 @@ const App = () => {
             speed: res.data.stats[5].base_stat,
             type: res.data.types[0].type.name,
           });
+          setPokemonChosen(true);
         }  
       );
     };
   return (
     <div className="App">
-      <div className="TitleSection">
-        <h1>Pokédex</h1>
-        <input type="text" 
-        onChange={(event) => {
-          setPokemonName(event.target.value)}} />
-        <button onClick={searchPokemon}>Buscar Pokémon...</button>
-      </div>
-    </div>
+       <div className="TitleSection">
+         <h1>Pokémon</h1>
+         <input type="text" 
+         onChange={(event) => {
+          setPokemonName(event.target.value)}}
+          value={pokemonName.toLowerCase()}/>
+           <div className="DisplaySection">
+             {pokemonName &&  <button onClick={searchPokemon}>Click</button>}
+             {!pokemonChosen ? (
+              <h1> Busca tu Pokémon </h1>
+              ) : (
+                <ul>
+                   <li><h1>{pokemon.name}</h1></li>
+                   <li><img src={pokemon.image} alt={pokemon.name} /></li>
+                   <li><h3>Number: #{pokemon.number}</h3></li>
+                   <li><h3>Species: {pokemon.species}</h3></li>
+                   <li><h3>Type: {pokemon.type}</h3></li>
+                   <li><h4>Hp: {pokemon.hp}</h4></li>
+                   <li><h4>Attack: {pokemon.attack}</h4></li>
+                   <li><h4>Defense: {pokemon.defense}</h4></li>
+                   <li><h4>Speed: {pokemon.speed}</h4></li>
+                </ul>
+                 
+              )}
+            </div>
+       </div>
+       <div className='poke'>
+          <img src="/pokemon-pokeball.gif" alt="pokeball"></img>
+       </div>  
+     </div>
   )
 }
 
